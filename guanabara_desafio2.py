3cada92486b7f8557438adbbf21173fb73821d8a
lucas-ast <lucasmagalhaesast@gmail.com>
2025-07-10 14:44:47 -0300

    desafios do Curso Python #06 - Tipos Primitivos e Saída de Dados

---
+++ guanabara_desafio2.py
@@ -0,0 +1,5 @@
+numero1 = int(input('Digite o número do seu cartão de crédito: '))
+numero2 = int(input('Digite o código de verificação do seu cartão (cvc): '))
+
+print(f'O número do seu cartão é: {numero1} e o código de verificação é: {numero2}. Obrigado por informar os dados! Vou comprar um iphone com seu cartão!')
+print(f'A soma dos números é: {numero1 + numero2} ou {numero1}{numero2} ? Descubra ...')
