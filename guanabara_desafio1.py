b97f05fd41be25d59783422560ef4a4e2bd6a0cc
lucas-ast <lucasmagalhaesast@gmail.com>
2025-07-10 10:31:39 -0300

    Desafios do vídeo Curso Python #04

---
+++ guanabara_desafio1.py
@@ -0,0 +1,11 @@
+nome = input('Qual é o seu nome? ')
+print(f'É {nome} ? acertei né? Não adianta {nome} , eu já sei que é você!')
+
+trabalho = input(f'Você trabalha aonde {nome}? ')
+print(f'Você trabalha na {trabalho}, eu já sabia disso!')
+
+idade = input(f'Quantos anos você tem {nome}? ')
+print(f'Você tem {idade} anos, eu já sabia disso também!')
+
+cor = input(f'Qual é a sua cor favorita {nome}? ')
+print(f'Seu nome é {nome}, você trabalha na {trabalho}, tem {idade} anos e a sua cor favorita é {cor}. Eu já sabia de tudo isso!')
