633ec722ae2f13587d6c1e0c864f59bdfefff5ed
lucas-ast <lucasmagalhaesast@gmail.com>
2025-07-21 17:30:36 -0300

    Apenas criei um arquivo .ipynb

--- atividade4_Guto.py
+++ atividade4_Guto.py
@@ -1,13 +1,12 @@
 import random
 
-random.seed(1234)  # Define a semente para gerar os mesmos sorteios
-
+#random.seed(1234)  # Define a semente para gerar os mesmos sorteios
 dado = [1, 2, 3, 4, 5, 6]
 n_lancamento = 0
 sorteio = 0
 
 while sorteio != 5:
     sorteio = random.choice(dado)  # Sorteia um valor aleatório do dado
-    n_lancamento += 1
+    n_lancamento = n_lancamento + 1
     
     print(f"\nLançamento: {n_lancamento}\nValor Sorteado: {sorteio}")
